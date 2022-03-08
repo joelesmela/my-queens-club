05f3cd3eb74f48573f9f31f502d311c68a22b1d1
Patricio García Millán <patogarciamillan@gmail.com>
2022-03-08 18:59:30 -0300

    Create Footer

---
+++ stories/components/Footer.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../../components/Footer/Footer';
+
+const Component = {
+  title: 'Footer',
+  component: Footer,
+};
+
+const Template = () => <Footer />;
+
+export const Default = Template.bind({});
+export default Component;
