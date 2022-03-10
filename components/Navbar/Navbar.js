7083e61a4cf67b7a3cb78f259a1c9e2d62c899cd
AlejandroBusi <alejandro_busi@hotmail.com>
2022-03-10 12:57:26 -0300

    add css module and buttons navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,31 +1,41 @@
 import Image from 'next/image';
-import './navbar.css';
+import styles from'./navbar.module.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bgNav">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-        <Image className='w-25 d-inline-block' width={100} height={100} src='/images/logo.png' alt='logo'/>
-        </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
+    <nav className={`navbar navbar-expand-lg navbar-dark ${styles.bgNav}`}>
+      <div className="container-fluid d-flex justify-content-between">
+        <div>
+          <a className="navbar-brand" href="#">
+            <Image className="w-25 d-inline-block" width={100} height={70} src='/images/logo.png' alt='logo'/>
+          </a>
+        </div>
+        <div className="d-flex justify-content-center" >
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          <div className="collapse navbar-collapse align-items-center" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className={`nav-link ${styles.colorLink}`} aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+              <li>
+                <button type="button" className="btn btn-success">Success</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <button type="button" className="btn btn-success">Success</button>
         </div>
       </div>
     </nav>
