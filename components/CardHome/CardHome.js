c2fc18fb75f5758ddc49e2f97815f84bc872f95e
Nico Gonzalez <53904695+Nidev-lab@users.noreply.github.com>
2022-03-11 22:05:11 -0300

    MQC-9-create-card-home (#6)
    Co-authored-by: Nicolas Gonzalez <ngonzalez@dminc.com>

---
+++ components/CardHome/CardHome.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import styles from './cardhome.module.css';
+
+const CardHome = ({ src, title }) => {
+  return (
+    <div className={`d-flex justify-content-center align-items-center ${styles.cardHome}`} style={{ backgroundImage: `url(${src})` }}>
+      <div className='text-center'>
+        <h2 className={`text-white mb-4 ${styles.titleName}`}>{title}</h2>
+        <button type="button" className={`btn btn-outline-light mt-1 ${styles.buttonStyle}`} >Ver Galerias</button>
+      </div>
+    </div>
+  );
+};
+
+CardHome.propTypes = {
+  src: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default CardHome;
