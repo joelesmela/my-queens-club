6112dfb33f94644e10a6fbd35b5a3ab9c39fdec2
Alejandro Busi <Alejandrobusi@gmail.com>
2022-03-19 21:59:28 -0300

    create component

---
+++ components/InfoSubs/InfoSubs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './infosubs.module.css';
+
+const InfoSubs = () => {
+  return (
+    <>
+      <h1>jaj</h1>
+    </>
+  );
+};
+
+export default InfoSubs;
