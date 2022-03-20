b9d02fbf69dfa9ec0f8bad0d46ded375453e5bc7
AlejandroBusi <alejandro_busi@hotmail.com>
2022-03-19 23:50:44 -0300

    added styles

--- components/InfoSubs/InfoSubs.js
+++ components/InfoSubs/InfoSubs.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from './infosubs.module.css';
 
 const InfoSubs = () => {
   return (
-    <>
-      <h1>jaj</h1>
-    </>
+    <div className={`d-flex justify-content-around ${styles.bgInfoSubs}`}>
+      <div>
+        <Link href="/"><a class={`px-3 ${styles.colorLink}`}><i class="bi bi-question-circle fs-5 pe-2"></i>¿Como me subscribo?</a></Link>
+      </div>
+      <div className={styles.simbol}>
+      </div>
+      <div>
+        <Link href="/" class=""><a class={`px-3 ${styles.colorLink}`}><i class="bi bi-person-plus fs-5 pe-2"></i> Registrarme</a></Link>
+      </div>
+    </div>
   );
 };
 
