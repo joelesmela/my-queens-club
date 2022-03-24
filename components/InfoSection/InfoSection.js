c0d452ecb4289bd390acef31b1197371e45b5c9c
AlejandroBusi <alejandro_busi@hotmail.com>
2022-03-24 19:30:01 -0300

    fix cursor pointer and label span

--- components/InfoSection/InfoSection.js
+++ components/InfoSection/InfoSection.js
@@ -5,28 +5,36 @@ const InfoSection = () => {
   return (
     <div className={`container py-2 ${styles.infoSectionContainer}`}>
       <div className='row justify-content-between'>
-        <div className={`col-sm-2 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
-          <i className={`bi bi-chat-square-text fs-1 ${styles.iconColor}`}></i>
+        <div className={`col-sm-2 col-md-6 col-lg-3 ${styles.box}`}>
           <Link href="/">
-            <spam className={styles.infoText}>ATENCIÓN AL CIENTE</spam>
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+              <i className={`bi bi-chat-square-text fs-1 ${styles.iconColor}`}></i>
+              <span className={styles.infoText}>ATENCIÓN AL CIENTE</span>
+            </div>
           </Link>
         </div>
-        <div className={`col-sm-12 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
-          <i className={`bi bi-cart4 fs-1 ${styles.iconColor}`}></i>
+        <div className={`col-sm-12 col-md-6 col-lg-3 ${styles.box}`}>
           <Link href="/">
-            <spam className={styles.infoText}>COMO COMPRAR</spam>
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+              <i className={`bi bi-chat-square-text fs-1 ${styles.iconColor}`}></i>
+              <span className={styles.infoText}>COMO COMPRAR</span>
+            </div>
           </Link>
         </div>
-        <div className={`col-sm-12 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
-          <i className={`bi bi-credit-card-2-back fs-1 ${styles.iconColor}`}></i>
+        <div className={`col-sm-12 col-md-6 col-lg-3 ${styles.box}`}>
           <Link href="/">
-            <spam className={styles.infoText}>MEDIOS DE PAGO</spam>
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+              <i className={`bi bi-chat-square-text fs-1 ${styles.iconColor}`}></i>
+              <span className={styles.infoText}>MEDIOS DE PAGO</span>
+            </div>
           </Link>
         </div>
-        <div className={`col-sm-12 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
-          <i className={`bi bi-truck fs-1 ${styles.iconColor}`}></i>
+        <div className={`col-sm-12 col-md-6 col-lg-3 ${styles.box}`}>
           <Link href="/">
-            <spam className={styles.infoText}>FORMAS DE ENVIO</spam>
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+              <i className={`bi bi-chat-square-text fs-1 ${styles.iconColor}`}></i>
+              <span className={styles.infoText}>FORMAS DE PAGO</span>
+            </div>
           </Link>
         </div>
       </div>
