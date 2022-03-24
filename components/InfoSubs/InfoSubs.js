f61afb05dc152ce5b1ae20d7bb4134ae57d5c752
Alejandro Busi <Alejandrobusi@gmail.com>
2022-03-24 18:45:17 -0300

    fix responsive

--- components/InfoSubs/InfoSubs.js
+++ components/InfoSubs/InfoSubs.js
@@ -4,14 +4,17 @@ import styles from './infosubs.module.css';
 
 const InfoSubs = () => {
   return (
-    <div className={`d-flex justify-content-around ${styles.bgInfoSubs}`}>
-      <div>
-        <Link href="/"><a class={`px-3 ${styles.colorLink}`}><i class="bi bi-question-circle fs-5 pe-2"></i>¿Como me subscribo?</a></Link>
-      </div>
-      <div className={styles.simbol}>
-      </div>
-      <div>
-        <Link href="/" class=""><a class={`px-3 ${styles.colorLink}`}><i class="bi bi-person-plus fs-5 pe-2"></i>Registrarme</a></Link>
+    <div className={`container-fluid ${styles.bgInfoSubs}`}>
+      <div className='row justify-content-between py-2'>
+        <div className='col-sm-12 col-md-12 col-lg-4 d-flex justify-content-center'>
+          <Link href="/"><a class={`${styles.colorLink}`}><i class="bi bi-question-circle fs-5 pe-2"></i>¿Como me subscribo?</a></Link>
+        </div>
+        <div className="col-lg-4 d-flex justify-content-center">
+          <div className={`d-none d-lg-block d-xl-none ${styles.simbol}`}/>
+        </div>
+        <div className='col-sm-12 col-md-12 col-lg-4 d-flex justify-content-center'>
+          <Link href="/" class=""><a class={`${styles.colorLink}`}><i class="bi bi-person-plus fs-5 pe-2"></i>Registrarme</a></Link>
+        </div>
       </div>
     </div>
   );
