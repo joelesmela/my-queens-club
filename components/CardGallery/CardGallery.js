8a6c64cecdac3722e9bdf4995bca5ef24d701494
Nico Gonzalez <53904695+Nidev-lab@users.noreply.github.com>
2022-03-25 16:36:42 -0300

    MQC-25-create-tab-component (#17)
    * MQC-25-create-tab-component
    
    * Update Tab
    
    * number to string in Image width and heigh
    
    Co-authored-by: Nicolas Gonzalez <ngonzalez@dminc.com>

--- components/CardGallery/CardGallery.js
+++ components/CardGallery/CardGallery.js
@@ -6,16 +6,18 @@ const CardGallery = ({
   src, title, alt, galleryName, imageQuantity,
 }) => {
   return (
-    <div className={styles.cardGallery}>
-      <div className='position-relative'>
-        <Image src={src} alt={alt} height="1031px" width="687px" layout="responsive" quality priority />
-        <div className='text-white px-2 py-1 bg-dark bg-opacity-75 position-absolute bottom-0 end-0 d-flex'>
-          <i className="bi bi-camera me-1" />
-          <div className={styles.imageQuantity}>{imageQuantity}</div>
+    <div className='p-1'>
+      <div className={styles.cardGallery}>
+        <div className='position-relative'>
+          <Image src={src} alt={alt} height={1031} width={687} layout="responsive" quality priority />
+          <div className='text-white px-2 py-1 bg-dark bg-opacity-75 position-absolute bottom-0 end-0 d-flex'>
+            <i className="bi bi-camera me-1" />
+            <div className={styles.imageQuantity}>{imageQuantity}</div>
+          </div>
         </div>
+        <p className={`text-white pt-3 pb-2 px-3 m-0 fw-normal fs-6 ${styles.titleName}`}>{galleryName}</p>
+        <p className={`text-secondary pt-1 pb-3 px-3 m-0 fw-light fs-6 ${styles.titleName}`}>{title}</p>
       </div>
-      <p className={`text-white pt-3 pb-2 px-3 m-0 fw-normal fs-6 ${styles.titleName}`}>{galleryName}</p>
-      <p className={`text-secondary pt-1 pb-3 px-3 m-0 fw-light fs-6 ${styles.titleName}`}>{title}</p>
     </div>
   );
 };
