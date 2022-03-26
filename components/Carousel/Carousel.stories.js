d86777bb5b8c201dfff08df05809953d2d004489
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-03-26 17:17:31 -0300

    Mobile carousel added

--- components/Carousel/Carousel.stories.js
+++ components/Carousel/Carousel.stories.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Carousel from './Carousel';
 import carouselInfo from './carouselData.example.json';
+import carouselInfoMobile from './carouselDataMobile.example.json';
 
 const Component = {
   title: 'Carousel',
   component: Carousel,
 };
 
-const Template = () => <Carousel carouselInfo={carouselInfo} />;
+const Template = () => <Carousel carouselInfo={carouselInfo} carouselInfoMobile={carouselInfoMobile} />;
 
 export const Default = Template.bind({});
 export default Component;
