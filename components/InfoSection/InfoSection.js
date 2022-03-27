b089baea6514c7bc5edf85853f73004cc5021641
Patricio García Millán <patogarciamillan@gmail.com>
2022-03-27 20:36:41 -0300

    Fix InfoSubs

--- components/InfoSection/InfoSection.js
+++ components/InfoSection/InfoSection.js
@@ -8,26 +8,26 @@ const InfoSection = ({ className }) => {
       <div className='row justify-content-between'>
         <div className={`col-sm-2 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
           <i className={`bi bi-chat-square-text fs-1 ${styles.iconColor}`}></i>
-          <Link href="/">
-            <spam className={styles.infoText}>ATENCIÓN AL CIENTE</spam>
+          <Link href="/" passHref>
+            <span className={styles.infoText}>ATENCIÓN AL CIENTE</span>
           </Link>
         </div>
         <div className={`col-sm-12 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
           <i className={`bi bi-credit-card-2-back fs-1 ${styles.iconColor}`}></i>
-          <Link href="/">
-            <spam className={styles.infoText}>MEDIOS DE PAGO</spam>
+          <Link href="/" passHref>
+            <span className={styles.infoText}>MEDIOS DE PAGO</span>
           </Link>
         </div>
         <div className={`col-sm-12 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
           <i className={`bi bi-question-circle fs-1 ${styles.iconColor}`}></i>
-          <Link href="/">
-            <spam className={styles.infoText}>PREGUNTAS FRECUENTES</spam>
+          <Link href="/" passHref>
+            <span className={styles.infoText}>PREGUNTAS FRECUENTES</span>
           </Link>
         </div>
         <div className={`col-sm-12 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
           <i className={`bi bi-person-fill fs-1 ${styles.iconColor}`}></i>
-          <Link href="/">
-            <spam className={styles.infoText}>MI CUENTA</spam>
+          <Link href="/" passHref>
+            <span className={styles.infoText}>MI CUENTA</span>
           </Link>
         </div>
       </div>
--- components/InfoSubs/InfoSubs.js
+++ components/InfoSubs/InfoSubs.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 import styles from './infosubs.module.css';
 
-const InfoSubs = () => {
+const InfoSubs = ({ className }) => {
   return (
-    <div className={`container-fluid ${styles.bgInfoSubs}`}>
-      <div className='row justify-content-between py-2'>
-        <div className='col-sm-12 col-md-12 col-lg-4 d-flex justify-content-center'>
-          <Link href="/"><a className={`${styles.colorLink}`}><i className="bi bi-question-circle fs-5 pe-2"></i>¿Como me subscribo?</a></Link>
+    <div className={`container-fluid w-100 w-md-50 ${className}`}>
+      <div className='h-100 row d-flex justify-content-center align-items-center'>
+        <div className='col-12 col-md-5 col-lg-4 d-flex justify-content-center'>
+          <Link href="/"><a className={`d-flex align-items-center fs-5 ${styles.colorLink}`}><i className="bi bi-question-circle fs-1 pe-4"></i>¿CÓMO ME SUSCRIBO?</a></Link>
         </div>
-        <div className="col-lg-4 d-flex justify-content-center">
-          <div className={`d-none d-lg-block d-xl-none ${styles.simbol}`}/>
-        </div>
-        <div className='col-sm-12 col-md-12 col-lg-4 d-flex justify-content-center'>
-          <Link href="/" class=""><a className={`${styles.colorLink}`}><i className="bi bi-person-plus fs-5 pe-2"></i>Registrarme</a></Link>
+        <div className={`d-none d-md-block ${styles.simbol}`}/>
+        <div className='col-12 col-md-5 col-lg-4 d-flex justify-content-center'>
+          <Link href="/"><a className={`d-flex align-items-center fs-5 ${styles.colorLink}`}><i className="bi bi-person-plus fs-1 pe-4"></i>REGISTRARME</a></Link>
         </div>
       </div>
     </div>
   );
+};
+
+InfoSubs.propTypes = {
+  className: PropTypes.string.isRequired,
 };
 
 export default InfoSubs;
