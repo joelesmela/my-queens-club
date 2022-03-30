32cefdc2de63aea512251d3b34c966ef9a9a7ec9
AlejandroBusi <alejandro_busi@hotmail.com>
2022-03-29 23:18:59 -0300

    change spanish leng and fix button collapse

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -8,31 +8,31 @@ const Navbar = () => {
         <a className="navbar-brand" href="#">
           <Image width={142.1} height={61} src='/images/logo.png' alt='logo' quality={100} />
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
+        <button className={`navbar-toggler ${styles.buttonCollapse}`}  type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span><i class={`fs-1 bi bi-justify ${styles.buttonCollIcon}`}></i></span>
         </button>
         <div className="collapse navbar-collapse flex-grow-0" id="navbarCollapse">
           <ul className="navbar-nav mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className={`nav-link ${styles.colorLink} px-0 px-md-3`} aria-current="page" href="#">Updates</a>
+              <a className={`nav-link ${styles.colorLink} px-0 px-md-3`} aria-current="page" href="#">Actualizaciones</a>
             </li>
             <li className="nav-item">
-              <a className={`nav-link ${styles.colorLink} px-0 px-md-3`} aria-current="page" href="#">Galleries</a>
+              <a className={`nav-link ${styles.colorLink} px-0 px-md-3`} aria-current="page" href="#">Galerias</a>
             </li>
             <li className="nav-item">
-              <a className={`nav-link ${styles.colorLink} px-0 px-md-3`} aria-current="page" href="#">Films</a>
+              <a className={`nav-link ${styles.colorLink} px-0 px-md-3`} aria-current="page" href="#">Videos</a>
             </li>
             <li className="nav-item">
               <a className={`nav-link ${styles.colorLink} px-0 px-md-3`} aria-current="page" href="#">Queens</a>
             </li>
             <li className="nav-item">
-              <a className={`nav-link ${styles.colorLink} px-0 px-md-3`} aria-current="page" href="#">Join the Club</a>
+              <a className={`nav-link ${styles.colorLink} px-0 px-md-3`} aria-current="page" href="#">Únete al club</a>
             </li>
             <li className="nav-item">
-              <button type="button" className={`nav-link ${styles.buttonStyleGet} px-4 mx-0 mx-md-4 my-2 my-md-0`}>Get started</button>
+              <button type="button" className={`nav-link ${styles.buttonStyleGet} px-4 mx-0 mx-md-4 my-2 my-md-0`}>Empezar</button>
             </li>
             <li className="nav-item">
-              <a className={`nav-link ${styles.colorLink}`} aria-current="page" href="#" data-bs-toggle="modal" data-bs-target="#singIn">Sign in</a>
+              <a className={`nav-link ${styles.colorLink}`} aria-current="page" href="#" data-bs-toggle="modal" data-bs-target="#singIn">iniciar sesión</a>
             </li>
           </ul>
         </div>
