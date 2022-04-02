a8edc8711fed14676ec2f4fccd6a4d508420a0a3
Nico Gonzalez <53904695+Nidev-lab@users.noreply.github.com>
2022-04-02 14:33:54 -0300

    MQC-43-create-page-filter-gallery-per-queen (#28)
    Co-authored-by: Nicolas Gonzalez <ngonzalez@dminc.com>

--- components/CardHome/CardHome.js
+++ components/CardHome/CardHome.js
@@ -1,15 +1,20 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 import styles from './cardhome.module.css';
 
-const CardHome = ({ src, title, alt }) => {
+const CardHome = ({
+  src, title, alt, id,
+}) => {
   return (
     <div className='p-1 position-relative'>
       <Image src={src} alt={alt} height={1920} width={1200} layout="responsive" quality={100} priority className={styles.cardHome} />
       <div className='text-center position-absolute h-100 w-100 d-flex justify-content-center align-items-center top-0'>
         <div className='text-white'>
           <h4 className={`mb-4 ${styles.titleName}`}>{title}</h4>
-          <button type="button" className={`btn btn-outline-light mt-1 ${styles.buttonStyle}`} >Ver Galerias</button>
+          <Link href={`/galleries/${id}`} passHref>
+            <button type="button" className={`btn btn-outline-light mt-1 ${styles.buttonStyle}`} >Ver Galerias</button>
+          </Link>
         </div>
       </div>
     </div>
@@ -17,6 +22,7 @@ const CardHome = ({ src, title, alt }) => {
 };
 
 CardHome.propTypes = {
+  id: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
