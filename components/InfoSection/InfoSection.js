52c7799a913ee901b28e3ff55b450e0ed60fc565
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-04-03 17:14:18 -0300

    MQC-38-create-generic-modal

--- components/InfoSection/InfoSection.js
+++ components/InfoSection/InfoSection.js
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 import styles from './infosection.module.css';
 import ModalSingIn from '../ModalSingIn/ModalSingIn';
+import GeneralModal from '../GeneralModal/GeneralModal';
+
+import data from '../../data/faq.json';
 
 const InfoSection = ({ className }) => {
   return (
@@ -8,24 +11,37 @@ const InfoSection = ({ className }) => {
       <div className='row justify-content-between'>
         <div className={`col-sm-2 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
           <i className={`bi bi-chat-square-text fs-1 ${styles.iconColor}`}></i>
-            <span>ATENCIÓN AL CIENTE</span>
+          <span>ATENCIÓN AL CIENTE</span>
         </div>
         <div className={`col-sm-12 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
           <i className={`bi bi-credit-card-2-back fs-1 ${styles.iconColor}`}></i>
-            <span>MEDIOS DE PAGO</span>
+          <span>MEDIOS DE PAGO</span>
         </div>
-        <div className={`col-sm-12 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
+        <div data-bs-toggle="modal" data-bs-target='#faqmodal' className={`col-sm-12 col-md-6 col-lg-3 d-flex flex-column justify-content-center align-items-center ${styles.box}`}>
           <i className={`bi bi-question-circle fs-1 ${styles.iconColor}`}></i>
-            <span>PREGUNTAS FRECUENTES</span>
+          <span>PREGUNTAS FRECUENTES</span>
         </div>
         <div className={`col-sm-12 col-md-6 col-lg-3 ${styles.box}`}>
           <div className='d-flex flex-column justify-content-center align-items-center' data-bs-toggle="modal" data-bs-target="#login">
             <i className={`bi bi-person-fill fs-1 ${styles.iconColor}`}></i>
-              <span>MI CUENTA</span>
+            <span>MI CUENTA</span>
           </div>
           <ModalSingIn isLogin idModal='login' activeLogin={1}/>
         </div>
       </div>
+
+      <GeneralModal id='faqmodal'>
+        <h3 className={`mb-4 ${styles.textColorSecondary}`}>Preguntas frecuentes</h3>
+
+        {
+          data.map((text, i) => (
+            <div key={i} className="mt-2">
+              <h6 className={styles.textColorPrimary}>{text.question}</h6>
+              <p className={styles.textColorSecondary}>{text.answer}</p>
+            </div>
+          ))
+        }
+      </GeneralModal>
     </div>
   );
 };
