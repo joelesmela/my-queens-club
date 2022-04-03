388941c265b3d9d1ec54598478aae07e1544e186
Patricio Daniel Garcia Millan <83433188+patoogm@users.noreply.github.com>
2022-04-02 22:40:13 -0300

    Update InfoSubs.js

--- components/InfoSubs/InfoSubs.js
+++ components/InfoSubs/InfoSubs.js
@@ -6,7 +6,7 @@ import ModalSingIn from '../ModalSingIn/ModalSingIn';
 
 const InfoSubs = ({ className }) => {
   return (
-    <div className={`container-fluid w-100 w-md-50  ${className}`}>
+    <div className={`container-fluid w-100 w-md-50 ${className}`}>
       <div className='h-100 row d-flex justify-content-center align-items-center'>
         <div className='col-12 col-md-5 col-lg-4 d-flex justify-content-center'>
           <Link href="/"><a className={`d-flex align-items-center fs-5 ${styles.colorLink}`}><i className="bi bi-question-circle fs-1 pe-4"></i>¿CÓMO ME SUSCRIBO?</a></Link>
