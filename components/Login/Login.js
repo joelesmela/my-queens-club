4658d524770fa77dee07ab0b2e0e64a7331ad4c8
Patricio García Millán <patogarciamillan@gmail.com>
2022-05-14 19:59:50 -0300

    Login Form

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import styles from './login.module.css';
 
 const Login = () => {
+  const [error, setError] = useState({});
   const {
     register, handleSubmit, formState: { errors },
   } = useForm();
@@ -16,30 +17,37 @@ const Login = () => {
       },
     });
     const json = await resp.json();
-    localStorage.setItem('accessToken', json.accessToken);
-    localStorage.setItem('user_name', json.name);
+    console.log(json);
+    if (json.status !== 400) {
+      localStorage.setItem('accessToken', json.accessToken);
+      localStorage.setItem('user_name', json.name);
+      window.location.reload(true);
+    } else {
+      setError(json);
+    }
   };
 
   return (
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="input-group mb-3">
           <span className={`input-group-text ${styles.icon}`}><i className="bi bi-person"></i></span>
-          <input type="text" className={`form-control ${styles.placeholder}`} placeholder="Nombre de usuario / Correo electrónico" aria-label="Email" {...register('user', { required: true })}/>
-          {errors.email && <span>Este campo es requerido</span>}
+          <input type="text" className={`form-control ${styles.placeholder}`} placeholder="Nombre de usuario / Correo electrónico *" aria-label="Email" {...register('user', { required: true })}/>
+          {errors.email && <p className={`${styles.text}`}>Este campo es requerido</p>}
         </div>
         <div className="input-group mb-3">
           <span className={`input-group-text ${styles.icon}`}><i className="bi bi-key"></i></span>
-          <input type="password" className={`form-control ${styles.placeholder}`} placeholder="Contraseña" aria-label="Password" {...register('password', { required: true })}/>
-          {errors.password && <span>Este campo es requerido</span>}
+          <input type="password" className={`form-control ${styles.placeholder}`} placeholder="Contraseña *" aria-label="Password" {...register('password', { required: true })}/>
         </div>
+          {errors.password && <p className={`${styles.text}`}>* Este campo es requerido</p>}
+          {error.mensaje && <p className={`${styles.text}`}>* Los datos son incorrectos</p>}
         <div className='d-flex justify-content-between'>
-          <div className="mb-3 form-check">
+          <div className="mb-3 pt-2 form-check">
             <input className={`form-check-input ${styles.checkBox}`} type="checkbox"/>
             <label className={`form-check-label ${styles.remember}`} htmlFor="flexCheckChecked">Recuérdame</label>
           </div>
-          <a className={`${styles.password}`}>¿Has olvidado la contraseña?</a>
+          <a className={`pt-2 ${styles.password}`}>¿Has olvidado la contraseña?</a>
         </div>
-        <button type="submit" className={`btn btn-primary w-100 ${styles.button}`}>ACCEDER</button>
+        <button type="submit" className={`btn w-100 ${styles.button}`}>ACCEDER</button>
       </form>
   );
 };
