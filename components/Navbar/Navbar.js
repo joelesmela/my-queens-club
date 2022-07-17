a73efabee2a93fdf952c43fe2a9d4e5e885690dc
Nico Gonzalez <53904695+Nidev-lab@users.noreply.github.com>
2022-07-16 21:21:29 -0300

    Update Navbar.js

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -61,8 +61,8 @@ const Navbar = () => {
                     <div className="dropdown">
                       <button className={`nav-link dropdown-toggle ${styles.buttonDropdown} px-4 px-lg-3 mx-0 my-2 my-md-0 mx-lg-3`} type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"></button>
                       <ul className={`dropdown-menu ${styles.dropdown}`} aria-labelledby="dropdownMenuButton1">
-                        <li><a className={`nav-link ${styles.colorLink} px-0 px-lg-3`} href="/user">Mi cuenta</a></li>
-                        <li><a className={`nav-link ${styles.colorLink} px-0 px-lg-3`} aria-current="page" onClick={logout}>Cerrar sesión</a></li>
+                        <li><Link className={`nav-link ${styles.colorLink} px-0 px-lg-3`} href="/user">Mi cuenta</Link></li>
+                        <li><Link className={`nav-link ${styles.colorLink} px-0 px-lg-3`} aria-current="page" onClick={logout}>Cerrar sesión</Link></li>
                       </ul>
                     </div>
                   )
