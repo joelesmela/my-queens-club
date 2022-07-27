a1862be8596a9789c0fe6a1cfaf35fb3e04562b3
cajimenez96 <carlosj_utn@hotmail.com>
2022-07-27 00:47:53 -0300

    add border in menu mobile

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -17,54 +17,91 @@ const Navbar = () => {
 
   return (
     <nav className={`navbar navbar-expand-lg ${styles.bgNav}`}>
-      <div className="container px-4 ">
+      <div className={`container-fluid d-flex justify-content-between ${styles.containerNav}`}>
         <Link href='/' passHref>
-          <Image width={147.6} height={61} src='/images/logo.png' alt='logo' quality={100} />
+          <Image className={styles.linkImage} width={147.6} height={61} src='/images/logo.png' alt='logo' quality={100} />
         </Link>
         <button className={`navbar-toggler ${styles.buttonCollapse}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <i className={`fs-1 bi bi-justify ${styles.buttonCollIcon}`}></i>
         </button>
-        <div className="collapse navbar-collapse flex-grow-0" id="navbarCollapse">
-          <ul className="navbar-nav mb-2 mb-lg-0 text-end">
-            <li className="nav-item">
+        <div className={`collapse navbar-collapse flex-grow-0 ${styles.collapseMenu}`} id="navbarCollapse">
+          <ul className={`navbar-nav d-flex ${styles.navCollapse} mb-2 mb-lg-0 text-end`}>
+            <li className={`nav-item ${styles.navItem}`}>
               <Link href="/" passHref>
                 <span className={`nav-link ${styles.colorLink} px-0 px-lg-3`}>Lo nuevo</span>
               </Link>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${styles.navItem}`}>
               <Link href='/galleries' passHref>
                 <a className={`nav-link ${styles.colorLink} px-0 px-lg-3`}>Galerías</a>
               </Link>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${styles.navItem}`}>
               <Link href='/queens' passHref>
                 <span className={`nav-link ${styles.colorLink} px-0 px-lg-3`}>Queens</span>
               </Link>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${styles.navItem}`}>
               <Link href="/" passHref>
                 <span className={`nav-link ${styles.colorLink} px-0 px-lg-3`}>Vídeos</span>
               </Link>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${styles.navItem}`}>
               <Link href="/join" passHref>
-                <span className={`nav-link ${styles.colorLink} px-0 px-md-3`}>Únete al club</span>
+                <span className={`nav-link ${styles.colorLink} px-0 px-lg-3`}>Únete al club</span>
               </Link>
             </li>
-            <li className="nav-item d-flex justify-content-end">
-              <button type="button" className={`nav-link ${styles.buttonStyleGet} px-4 px-lg-3 mx-0 my-2 my-md-0 mx-lg-3`}>Empezar</button>
+            <li className={`nav-item ${styles.navItem}`}>
+              <button type="button" className={`nav-link ${styles.buttonStyleGet} px-4 px-lg-3 mb-3 mb-lg-0 me-0 me-lg-3`}>Empezar</button>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${styles.navItem}`}>
               {
                 userData.accessToken
                   ? (
                     <div className="dropdown">
-                      <button className={`nav-link dropdown-toggle ${styles.buttonDropdown} px-4 px-lg-3 mx-0 my-2 my-md-0 mx-lg-3`} type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                      <button className={`nav-link dropdown-toggle ${styles.buttonDropdown} px-5 px-lg-3`} type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"></button>
                       <ul className={`dropdown-menu ${styles.dropdown}`} aria-labelledby="dropdownMenuButton1">
-                        <li><Link className={`nav-link ${styles.colorLink} px-0 px-lg-3`} href="/user">Mi cuenta</Link></li>
-                        <li><span className={`nav-link ${styles.colorLink} px-0 px-lg-3`} aria-current="page" onClick={logout}>Cerrar sesión</span></li>
+                        <li>
+                          <Link className="nav-link" href="/user" passHref>
+                            <span className={`${styles.colorLink} m-3`}>Mi cuenta</span>
+                          </Link>
+                        </li>
+                        <li>
+                          <span className={`${styles.colorLink} m-3`} aria-current="page" onClick={logout}>Cerrar sesión</span>
+                        </li>
                       </ul>
                     </div>
+                    // <>
+                    //   <div className="btn-group dropstart">
+                    //     <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    //       Dropstart
+                    //     </button>
+                    //     <ul className="dropdown-menu">
+                    //     </ul>
+                    //   </div>
+
+                    //   <div className="btn-group">
+                    //     <div className="btn-group dropstart" role="group">
+                    //       <button type="button" className="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                    //         <span className="visually-hidden">Toggle Dropstart</span>
+                    //       </button>
+                    //       <ul className="dropdown-menu">
+                    //         <li></li>
+                    //       </ul>
+                    //     </div>
+                    //     <button type="button" className="btn btn-secondary">
+                    //       Split dropstart
+                    //     </button>
+                    //   </div>
+
+                    //   <div class="btn-group dropend">
+                    //     <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    //       Dropright
+                    //     </button>
+                    //     <ul class="dropdown-menu">
+                    //     </ul>
+                    //   </div>
+                    // </>
                   )
                   : (
                       <span className={`nav-link ${styles.colorLink} px-0 px-lg-3`} aria-current="page" data-bs-toggle="modal" data-bs-target="#singIn" >Iniciar sesión</span>
